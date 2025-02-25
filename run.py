87da1a839eef8d4d65a17a8c0683965b9dec2760
Kritika Datar <kritikadatarss@gmail.com>
2025-02-25 23:15:19 +0530

    Added run config file

--- run.py
+++ run.py
@@ -1,33 +1,41 @@
 import os
 import pickle
-import yaml
-import click
-import chromadb
 
-from ICRQE.workspace.summarize import summarize_file
-from ICRQE.llm.llm_client import build_llm_completion_client, build_llm_chat_client
-from ICRQE.storehouse.chromadb_utils import init_db, populate_db
-from ICRQE.tools.tools_to_yaml import setup_and_load_yaml, setup_tools, save_tools_to_yaml
+import chromadb
+import click
+import yaml
+
 from ICRQE.agent.agent import Agent
+from ICRQE.llm.build_llm_client import (build_llm_chat_client,
+                                        build_llm_completion_client)
+from ICRQE.storehouse.chromadb_utils import init_db, populate_db
+from ICRQE.tools.tools_to_yaml import (save_tools_to_yaml, setup_and_load_yaml,
+                                       setup_tools)
+from ICRQE.workspace.summarize import summarize_file
 from src.ICRQE.dataclasses_v1 import Config
 
 
 @click.command()
-@click.option('--config', '-c', default='config.yaml', help='Path to the config yaml file')
-@click.option('--model', '-m', default='gpt-4-0125-preview', help='The name of the LLM model to use')
-@click.option('--prompts_file', '-p', default=None, help='Path to the prompts file')
-def main(config: str, model: str, prompts_file: str | None):
+@click.option(
+    "--config", "-c", default="config.yaml", help="Path to the config yaml file"
+)
+@click.option(
+    "--model",
+    "-m",
+    default="gpt-4-0125-preview",
+    help="The name of the LLM model to use",
+)
+def main(config: str, model: str):
     """
     Main run script
 
     Args:
         config (str): Path to the config yaml file
         model (str): The name of the LLM model to use
-        prompts_file (str | None): Path to the prompts file
     """
 
     # Load configuration
-    with open(config, 'r') as f:
+    with open(config, "r") as f:
         config_data = yaml.safe_load(f)
     config = Config(**config_data)
 
@@ -38,41 +46,45 @@ def main(config: str, model: str, prompts_file: str | None):
     client = chromadb.PersistentClient(config.db_path)
 
     # Initialize the database and get a list of files in the repo
-    collection, files = init_db(client, os.path.expanduser(config.repository), config.extensions)
+    collection, files = init_db(
+        client, os.path.expanduser(config.repository), config.extensions
+    )
     files = sorted(files)
 
     # LLM that takes a string as input and returns a string
     llm = build_llm_completion_client(model)
 
     # File for storing LLM generated descriptions of files, functions, and classes
-    descriptions_file = os.path.join(config.cache_dir, 'descriptions.pkl')
+    descriptions_file = os.path.join(config.cache_dir, "descriptions.pkl")
 
     # Load existing descriptions if available
     descriptions = {}
     if os.path.exists(descriptions_file):
-        with open(descriptions_file, 'rb') as f:
+        with open(descriptions_file, "rb") as f:
             descriptions = pickle.load(f)
 
     # Generate summaries for files, classes, and functions
     for filepath in files:
-        summarize_file(filepath, os.path.expanduser(config.repository), llm, descriptions)
+        summarize_file(
+            filepath, os.path.expanduser(config.repository), llm, descriptions
+        )
 
     # Save the descriptions to a file in the cache directory
-    with open(descriptions_file, 'wb') as f:
+    with open(descriptions_file, "wb") as f:
         pickle.dump(descriptions, f)
 
     # Populate the database with the descriptions
     populate_db(descriptions, collection)
 
     # Load or generate Tools
-    tools_filepath = os.path.join(config.cache_dir, 'tools.yaml')
-    tool_descriptions = setup_and_load_yaml(tools_filepath, 'tools')
+    tools_filepath = os.path.join(config.cache_dir, "tools.yaml")
+    tool_descriptions = setup_and_load_yaml(tools_filepath, "tools")
     tools = setup_tools(
         config.Tools,
         tool_descriptions,
         collection,
         llm,
-        os.path.expanduser(config.repository)
+        os.path.expanduser(config.repository),
     )
     save_tools_to_yaml(tools, tools_filepath)
 
@@ -82,5 +94,5 @@ def main(config: str, model: str, prompts_file: str | None):
     agent.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
